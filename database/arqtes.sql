54deacf1c68681b29df75d4d2fd1e9587c4b082a
SaraPasolini <saraalmeida3606@gmail.com>
2025-10-06 15:38:16 -0300

    Banco de dados populado

--- database/arqtes.sql
+++ database/arqtes.sql
@@ -1,3 +1,45 @@
-CREATE test(
+CREATE TABLE Curso(
+  cod_Curso int PRIMARY KEY
+  num_Ano int
+  cod_IES int
+  cod_OraganizacaoAcademica int
+  cod_Grupo int
+  cod_Modaliade int
+  cod_Municipio_Curso int
+  cod_UF_Curso int
+  cod_Regiao_Curso int
+
+)
+
+
+CREATE TABLE Estudante (
+    id_Estudante int PRIMARY KEY
+    num_ano int
+    cod_curso int FOREIGN KEY
+    sexo char
+    num_Idade int 
+    Question_101 char
+    Question_126 char
+)
+
+CREATE TABLE Prova (
+    id_Prova int PRIMARY KEY
+    num_Ano int
+    cod_Curso FOREIGN KEY
+    tp_Pres int
+    Notas int 
+    NT_GER 
+    NT_FG 
+    NT_CE 
+    -- Perguntar a professora sobre o tipo das variaveis ( se é int ou char)
+
+
+) 
+
+CREATE TABLE PercepcaoProva (
+    id_Percepcao int PRIMARY KEY
+    num_Ano int
+    cod_Curso FOREIGN KEY
+    Resposta str -- string  
     
 )
