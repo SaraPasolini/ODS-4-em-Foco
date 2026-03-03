188b67dce83d79d4318eda8c757f3ae005530e7d
Gabriel Henrique <gabrielhss1677@gmail.com>
2026-03-03 19:45:01 -0300

    Update acessaenade.cy.js

--- cypress/e2e/acessaenade.cy.js
+++ cypress/e2e/acessaenade.cy.js
@@ -1,7 +1,6 @@
 describe('Baixar microdados do Enade por ano', () => {
 
   before(() => {
-    // Ignora erros JS do site do governo
     Cypress.on('uncaught:exception', () => false);
   });
 
@@ -12,27 +11,32 @@ describe('Baixar microdados do Enade por ano', () => {
     cy.contains('Microdados do Enade')
       .should('be.visible');
 
-    // percorre todos os links da página
     cy.get('a').each(($link) => {
 
       const texto = $link.text().trim();
 
-      // filtra apenas os links de microdados
       if (texto.startsWith('Microdados do Enade')) {
 
         const url = $link.prop('href');
         const ano = texto.replace('Microdados do Enade', '').trim();
 
         if (url && ano) {
+
           cy.log(`Baixando Microdados do Enade ${ano}`);
 
-          // chama o Node.js para baixar o arquivo
-          cy.task('downloadFile', { url });
+          const fileName = `enade_${ano}.zip`;
+
+          cy.task('downloadFile', { url, fileName })
+            .then(() => {
+
+              cy.task('unzipFile', {
+                fileName: fileName
+              });
+
+            });
         }
       }
     });
+
   });
 });
-
-
-
